43dd635430cc7c23c169cc171c9adb3a9987d86a
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-23 16:44:54 -0500

    add/ challenge 10 done

--- src/screens/ChallengeScreen.tsx
+++ src/screens/ChallengeScreen.tsx
@@ -16,27 +16,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#28425B',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   purpleBox: {
-    alignItems: 'stretch',
-
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#5856D6',
     width: 100,
+    height: 100,
   },
   orangeBox: {
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#F0A23B',
     width: 100,
+    height: 100,
+    top: 50,
   },
   blueBox: {
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#28C4D9',
     width: 100,
+    height: 100,
   },
 });
