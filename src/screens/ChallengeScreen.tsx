a2bd99a981bc4a58481c599da23f75880e4d1bb9
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-23 16:24:37 -0500

    add/ challenge 1 done

--- src/screens/ChallengeScreen.tsx
+++ src/screens/ChallengeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 export const ChallengeScreen = () => {
   return (
@@ -27,6 +27,7 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#F0A23B',
+    flex: 1,
     width: 100,
     height: 100,
   },
