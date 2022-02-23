5e052ee50094a34868ca3819256ed0f2ea085f8f
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-23 16:35:58 -0500

    add/ challenge 5 done

--- src/screens/ChallengeScreen.tsx
+++ src/screens/ChallengeScreen.tsx
@@ -15,29 +15,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28425B',
+    flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'stretch',
   },
   purpleBox: {
-    alignSelf: 'flex-end',
+    alignItems: 'stretch',
+
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#5856D6',
     width: 100,
-    height: 100,
   },
   orangeBox: {
-    alignSelf: 'center',
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#F0A23B',
     width: 100,
-    height: 100,
   },
   blueBox: {
     borderWidth: 10,
     borderColor: 'white',
     backgroundColor: '#28C4D9',
     width: 100,
-    height: 100,
   },
 });
